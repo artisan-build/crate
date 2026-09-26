6cbb74f3f45b3a42b0c9e42842921601a4ef7d4c
Ed Grosvenor <ed@artisan.build>
2026-09-26 14:47:00 +0200

    Apply composer ready updates

--- tests/Feature/BuiltForCloudConformanceTest.php
+++ tests/Feature/BuiltForCloudConformanceTest.php
@@ -11,6 +11,7 @@ use ArtisanBuild\BuiltForCloud\Commands\CredentialListCommand;
 use ArtisanBuild\BuiltForCloud\Commands\CredentialMintCommand;
 use ArtisanBuild\BuiltForCloud\Commands\CredentialRevokeCommand;
 use ArtisanBuild\BuiltForCloud\Commands\CredentialRotateCommand;
+use ArtisanBuild\BuiltForCloud\Commands\FreshCommand;
 use ArtisanBuild\BuiltForCloud\Commands\HmacRewrapCommand;
 use ArtisanBuild\BuiltForCloud\Commands\InstallOperatorCredentialCommand;
 use ArtisanBuild\BuiltForCloud\Commands\OutboxDrainCommand;
@@ -103,7 +104,7 @@ it('passes the unified auth consumer conformance spec', function (): void {
             CredentialMintCommand::class,
             CredentialRevokeCommand::class,
             CredentialRotateCommand::class,
-            'ArtisanBuild\BuiltForCloud\Commands\FreshCommand',
+            FreshCommand::class,
             HmacRewrapCommand::class,
             InstallOperatorCredentialCommand::class,
             OutboxDrainCommand::class,
